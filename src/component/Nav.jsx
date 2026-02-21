319d10cd18aa659bbce76ec484a57727f9b2345c
Yaoxi Yang <yyan0747@uni.sydney.edu.au>
2026-02-22 10:46:16 +1100

    Update Nav.jsx

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -25,15 +25,15 @@ export default function Nav({menuOpen, toggleMenu}) {
                   </li>
 
                   <li className='link-style text-t4-desktop'>
-                    <NavLink to='/'>Home</NavLink>
+                    <NavLink onClick={toggleMenu} to='/'>Home</NavLink>
                   </li>
 
                   <li className='link-style text-t4-desktop'>
-                    <NavLink to='/recipes'>Recipes</NavLink>
+                    <NavLink onClick={toggleMenu} to='/recipes'>Recipes</NavLink>
                   </li>
 
                   <li className='link-style text-t4-desktop'>
-                    <NavLink to='/chef'>AI Chef</NavLink>
+                    <NavLink onClick={toggleMenu} to='/chef'>AI Chef</NavLink>
                   </li>
                 </ul>
               </div>         
